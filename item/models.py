c3ed1c5fcd5881d2142e068f588655957a532416
khushi <khushi@gmail.com>
2024-02-13 17:17:07 +0530

    img

--- item/models.py
+++ item/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name=models.CharField(max_length=255)
@@ -9,6 +10,20 @@ class Category(models.Model):
 
         def __str__(self):
             return self.name 
+        
+class Item(models.Model):
+    category=models.ForeignKey(Category,related_name='items',on_delete=models.CASCADE)
+    name=models.CharField(max_length=255)
+    description=models.TextField(blank=True,null=True)
+    price=models.FloatField()
+    image=models.ImageField(upload_to='item_images',blank=True,null=True)
+    is_sold=models.BooleanField(default=False)
+    created_by=models.ForeignKey(User,related_name='items',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name 
+    
+    
         
 
 
