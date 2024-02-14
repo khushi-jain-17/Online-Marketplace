477ecf54a10dd47131a5d6b465bc7bf6c864112e
khushi <khushi@gmail.com>
2024-02-14 18:02:42 +0530

    'dash'

--- item/views.py
+++ item/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .forms import NewItemForm,EditItemForm 
 
+def detail(request,pk):
+    item=get_object_or_404(Item,pk=pk)
+    related_items=Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
+    return render(request,'item/detail.html',{'item':item,'related_items':related_items})
+
 @login_required
 def new(request):
     if request.method=='POST':
@@ -20,9 +25,6 @@ def new(request):
 def items(request):
     pass
 
-def detail(request,pk):
-    item=get_object_or_404(Item,pk=pk)
-    related_items=Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
-    return render(request,'item/detail.html',{'item':item,'related_items':related_items})
+
 
 
--- puddle/urls.py
+++ puddle/urls.py
@@ -6,7 +6,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
-    path('items/',include('item.urls'))
+    path('items/',include('item.urls')),
+    path('dashboard/',include('dashboard.urls'))
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
